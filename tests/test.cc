3a22077355e550f49bf441725fd0294220a43cbe
TonyChen <tony@gmail.com>
2024-03-23 14:46:09 +0800

    more make sense template

--- tests/test.cc
+++ tests/test.cc
@@ -1,7 +1,30 @@
 #include <gtest/gtest.h>
+using namespace std;
+// using namespace testing;
+
+#define TEST_FUNC Solution::threeSum
+class Solution {
+public:
+    static int threeSum(int nums)
+    {
+      return nums;
+    }
+};
+
+class LeetCodeTester : public testing::TestWithParam<tuple<int,int>> {
+ public:
+  void SetUp() override {  
+  }
+  void TearDown() override {
+  }
+};
+
+INSTANTIATE_TEST_SUITE_P(TestInput1, LeetCodeTester,
+                         testing::Combine(testing::Values(10), testing::Values(10))
+                         );
 
 
-TEST(FactorialTest, Negative) {
-  EXPECT_EQ(1, 1);
-  EXPECT_EQ(1, 1);
+TEST_P( LeetCodeTester, TestInputPattern1) {
+  auto [input ,output] = GetParam();
+  EXPECT_EQ(output, TEST_FUNC(input));
 }
