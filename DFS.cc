b1c6e879b4dd0a38c73885faf40115a07eb5531b
TonyChen <tony@gmail.com>
2024-04-13 17:57:41 +0800

    add DFS good example

---
+++ DFS.cc
@@ -0,0 +1,46 @@
+#include <vector>
+#include <list>
+#include <stack>
+using namespace std;
+#define FALSE 0
+#define TRUE 1
+#define MAX_INT 0xFFFFFFFF
+
+
+namespace myTest{
+    bool detectCycle(int src ,vector<int>&vis,vector<int>&crvisit,vector<int> adj[]){
+        vis[src]=1;
+        crvisit[src]=1;
+        for(auto it  : adj[src]){
+            if(!vis[it]){
+                if(detectCycle(it , vis , crvisit , adj)) return true ;
+            }
+            else if(vis[it]  && crvisit[it]){
+                return true ;
+            }
+        }
+        crvisit[src]=0;
+        return false ;
+    }
+    bool canFinish(int numCourses, vector<vector<int>>& pq) {
+        int n = numCourses;   
+        vector<int> adj[n];
+        for(auto x : pq){
+            vector<int> data = x;
+            int a =data[0];
+            int b = data[1];
+            adj[a].push_back(b);
+            
+        }
+        vector<int> vis(n,0);
+        vector<int> crvisit(n,0);
+        for(int i=0;i<n;i++){
+            if(!vis[i]){
+                if(detectCycle(i,vis,crvisit ,adj))
+                return false ;
+            }
+        }
+        return true ;
+    }
+
+};
--- tests/test.cc
+++ tests/test.cc
@@ -37,12 +37,13 @@ INSTANTIATE_TEST_SUITE_P(TestInput1, LeetCodeTester,
                                 // std::make_tuple(TRUE, 1, std::vector<std::vector<int>>{}),
                                 // std::make_tuple(FALSE, 5, std::vector<std::vector<int>>{{0, 1},{0, 2},{2, 3},{3, 4},{4, 2}}),
                                 // std::make_tuple(TRUE, 5, std::vector<std::vector<int>>{{0, 1},{0, 2},{2, 3},{3, 4},{1, 4}}),
-                                std::make_tuple(TRUE, 6, std::vector<std::vector<int>>{{0, 1},{0, 2},{2, 3},{3, 4},{3, 5},{5, 1}})
+                                // std::make_tuple(TRUE, 6, std::vector<std::vector<int>>{{0, 1},{0, 2},{2, 3},{3, 4},{3, 5},{5, 1}})
+                                std::make_tuple(TRUE, 4, std::vector<std::vector<int>>{{0, 1},{1, 2},{2, 3},{0, 3}})
                             )
                          );
 
 
 TEST_P( LeetCodeTester, TestInputPattern1) {
   auto [input, n, output] = GetParam();
-  EXPECT_EQ(input, myTest::course_schdule(n, output));
+  EXPECT_EQ(input, myTest::canFinish(n, output));
 }
